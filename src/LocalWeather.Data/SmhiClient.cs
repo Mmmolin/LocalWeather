616d0ba46708fefbb1cfbbe7fb96026a47e07b29
unknown <mmmolin78@gmail.com>
2018-06-20 00:32:12 +0200

    Add weather data GetAsync

---
+++ src/LocalWeather.Data/SmhiClient.cs
@@ -0,0 +1,19 @@
+﻿using LocalWeather.Domain;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LocalWeather.Data
+{
+    public class SmhiClient
+    {
+        public async Task<Forecast> GetForecastAsync()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/12.275/lat/57.7386/data.json");
+            string content = await response.Content.ReadAsStringAsync();
+            Forecast forecast = JsonConvert.DeserializeObject<Forecast>(content);
+            return (forecast);
+        }
+    }
+}
--- src/LocalWeather/Controllers/HomeController.cs
+++ src/LocalWeather/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LocalWeather.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using LocalWeather.Domain;
 
 namespace LocalWeather.Controllers
 {
@@ -12,7 +15,10 @@ namespace LocalWeather.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var smhi = new LocalWeather.Data.SmhiClient();
+            var response = smhi.GetForecastAsync();
+            Forecast test = response.Result;
+            return View(response);
         }
 
         public IActionResult About()

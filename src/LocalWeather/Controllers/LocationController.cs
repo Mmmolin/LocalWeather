1e6fae989f8406f2472bbf2118ff61eba63895a8
unknown <mmmolin78@gmail.com>
2018-08-15 00:40:45 +0200

    Add CSS, delete SearchAction, Add Fonts

--- src/LocalWeather/Controllers/LocationController.cs
+++ src/LocalWeather/Controllers/LocationController.cs
@@ -10,107 +10,12 @@ namespace LocalWeather.Controllers
 {
     public class LocationController : Controller
     {
-        public ActionResult SearchLocation()
-        {
-            return View();
-        }
-
         [HttpPost]
         public async Task<ActionResult> GetLocation(string textBoxSearch)
         {            
             var locationData = new LocalWeather.Data.LocationData();
             var locations = await locationData.ListLocation(textBoxSearch);                 
             return View(locations);
-        }
-
-        public ActionResult SetLocation()
-        {            
-            return View();
-        }
-        
-
-
-
-
-        // GET: Location
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Location/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Location/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Location/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Location/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Location/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Location/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Location/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        }        
     }
 }

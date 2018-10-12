48e6da327b8c28e6250e2c14f2f249143aa651be
unknown <mmmolin78@gmail.com>
2018-10-12 22:05:43 +0200

    Add filter Swedish Locations

---
+++ src/LocalWeather.Business/LocationData.cs
@@ -0,0 +1,45 @@
+﻿using LocalWeather.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocalWeather.Business
+{
+    public class LocationData
+    {
+        private List<Location> _locations;
+
+        public LocationData()
+        {
+            _locations = new List<Location>();
+        }
+
+        public void SetSwedishLocations(List<Location> locations)
+        {
+            var SwedishLocations = new List<Location>();
+            if(!IsNullOrEmpty(locations))
+            {
+                foreach (var location in locations)
+                {
+                    var splitLocation = location.Display_Name.Split(",");
+                    var country = splitLocation[splitLocation.Length - 1].Trim();
+                    if (country == "Sverige")
+                    {
+                        SwedishLocations.Add(location);
+                    }
+                }
+            }
+            this._locations = SwedishLocations;
+        }
+
+        public List<Location> GetSwedishLocations()
+        {
+            return _locations;
+        }
+
+        private bool IsNullOrEmpty(List<Location> locations)
+        {
+            return (locations == null || locations.Count == 0);
+        }
+    }
+}
